c7ae815a7df1b59424fc04d0fa1ce430a315eded
i14nakatukasa <i14nakatukasa@gmail.com>
2023-01-10 00:07:22 +0900

    headingも写真をかえたけど同じ写真になる

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -7,37 +7,37 @@ import Button from '@material-ui/core/Button';
 
 export default function ImageCard() {
 
-    const getRandom = (min: number=0, max: number=1000): number => {
-        var random = Math.floor( Math.random() * (max + 1 - min) ) + min;
-        return random;
-    }
+  const getRandom = (min: number = 0, max: number = 1000): number => {
+    var random = Math.floor(Math.random() * (max + 1 - min)) + min;
+    return random;
+  }
 
-    return (
-        <Card
-        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-      >
-        <CardMedia
-          component="img"
-          sx={{
-            // 16:9
-            pt: '56.25%',
-          }}
-          image={"https://source.unsplash.com/random?" + getRandom()}
-          alt="random"
-        />
-        <CardContent sx={{ flexGrow: 1 }}>
-          <Typography gutterBottom variant="h5" component="h2">
-            Heading
-          </Typography>
-          <Typography>
-            This is a media card. You can use this section to describe the
-            content.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">View</Button>
-          <Button size="small">Edit</Button>
-        </CardActions>
-      </Card>
-    )
+  return (
+    <Card
+      sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+    >
+      <CardMedia
+        component="img"
+        sx={{
+          // 16:9
+          pt: '56.25%',
+        }}
+        image={"https://source.unsplash.com/user/i14nakatukasa"}
+        alt="random"
+      />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom variant="h5" component="h2">
+          Heading
+        </Typography>
+        <Typography>
+          This is a media card. You can use this section to describe the
+          content.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">View</Button>
+        <Button size="small">Edit</Button>
+      </CardActions>
+    </Card>
+  )
 }
